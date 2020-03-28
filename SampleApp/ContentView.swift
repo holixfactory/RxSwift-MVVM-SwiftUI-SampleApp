b4ef56b58802f3cf86dce49f8b99e1e588121b7c
momamene <momamene@gmail.com>
2020-03-28 16:39:47 +0900

    Initial Commit

---
+++ SampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleApp
+//
+//  Created by Hyuntak Joo on 2020/03/28.
+//  Copyright © 2020 Hyuntak Joo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
